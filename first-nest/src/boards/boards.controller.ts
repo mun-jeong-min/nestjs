05c0f275a02f4c927031cbaf6ce3611e22fb786a
1406MJM <3330kys@dsm.hs.kr>
2021-11-18 14:12:12 +0900

    [ADD] boardModel

--- first-nest/src/boards/boards.controller.ts
+++ first-nest/src/boards/boards.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
     constructor(private boardsService: BoardsService){}
 
-@Get()
-getAllBoard(){
+@Get('/')
+getAllBoard(): Board[]{ // interface 시에는 배열 표시를 위해 타입지정 뒤에 [] 붙여준다
     return this.boardsService.getAllBoards();
 }
 }
