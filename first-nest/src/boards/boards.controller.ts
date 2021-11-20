fb1c9b1f1b45141dcb9f437adbe365248e47f6b5
1406MJM <3330kys@dsm.hs.kr>
2021-11-20 17:01:43 +0900

    [ADD] uuid를 이용한 게시물 삭제

--- first-nest/src/boards/boards.controller.ts
+++ first-nest/src/boards/boards.controller.ts
@@ -1,26 +1,26 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
-    constructor(private boardsService: BoardsService){}
+  constructor(private boardsService: BoardsService) {}
 
-@Get('/')
-getAllBoard(): Board[]{
+  @Get('/')
+  getAllBoard(): Board[] {
     return this.boardsService.getAllBoards();
-}
-@Get('/:id')
-findId(
-    @Param('id') id:string
-): Board{
-    return this.boardsService.findBoardById(id)
-}
-@Post('/')
-createBoard(
-    @Body() createBoardDto: CreateBoardDto
-): Board {
+  }
+  @Get('/:id')
+  findId(@Param('id') id: string): Board {
+    return this.boardsService.findBoardById(id);
+  }
+  @Post('/')
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
     return this.boardsService.createBoard(createBoardDto);
-}
+  }
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+      this.boardsService.deleteBoard(id)
+  }
 }
--- first-nest/src/boards/boards.service.ts
+++ first-nest/src/boards/boards.service.ts
@@ -1,29 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
-import { v1 as uuid} from 'uuid'
+import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
-    private boards:Board[] = [];
+  private boards: Board[] = [];
 
-    getAllBoards(): Board[]{
-        return this.boards;
-    }
-    
-    createBoard(createBoardDto: CreateBoardDto){
-       const {title,description} = createBoardDto;
-        const board:Board = {
-            id: uuid(),
-            title,
-            description,
-            status: BoardStatus.PUBLIC
-        }
-        this.boards.push(board);
-        return board;
-    }
+  public getAllBoards(): Board[] {
+    return this.boards;
+  }
 
-    findBoardById(id:string): Board{
-        return this.boards.find((board) => board.id === id)
-    }
+  public createBoard(createBoardDto: CreateBoardDto) {
+    const { title, description } = createBoardDto;
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+    this.boards.push(board);
+    return board;
+  }
+
+  public findBoardById(id: string): Board {
+      return this.boards.find((board) => board.id === id)
+  }
+
+  public deleteBoard(id: string): void {
+      this.boards = this.boards.filter((board) => board.id !== id)
+  }
 }
